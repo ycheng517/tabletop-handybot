221f01d8b0541bb53382acfdb6228c3b3af807e3
Yifei Cheng <ycheng517@gmail.com>
2024-05-04 17:04:26 -0400

    working towards gripper control

--- grounded_sam_ros/grounded_sam_ros/grounded_sam_node.py
+++ grounded_sam_ros/grounded_sam_ros/grounded_sam_node.py
@@ -1,6 +1,6 @@
 import json
+import os
 
-import cv2
 import numpy as np
 import open3d as o3d
 import openai
@@ -8,6 +8,7 @@ import rclpy
 import supervision as sv
 import torch
 import torchvision
+import cv2
 from cv_bridge import CvBridge
 from groundingdino.util.inference import Model
 from openai.types.beta import Assistant
@@ -15,6 +16,11 @@ from rclpy.node import Node
 from segment_anything import SamPredictor, sam_model_registry
 from sensor_msgs.msg import Image, PointCloud2, PointField
 from std_msgs.msg import Header, String
+from geometry_msgs.msg import Pose, PoseStamped
+from moveit.planning import MoveItPy
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+from pymoveit2 import GripperInterface
 
 from .openai_assistant import get_or_create_assistant
 
@@ -23,8 +29,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # GroundingDINO config and checkpoint
 GROUNDING_DINO_CONFIG_PATH = "/home/yifei/src/Grounded-Segment-Anything/GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
 GROUNDING_DINO_CHECKPOINT_PATH = (
-    "/home/yifei/src/Grounded-Segment-Anything/groundingdino_swint_ogc.pth"
-)
+    "/home/yifei/src/Grounded-Segment-Anything/groundingdino_swint_ogc.pth")
 
 # Segment-Anything checkpoint
 SAM_ENCODER_VERSION = "vit_h"
@@ -38,13 +43,13 @@ NMS_THRESHOLD = 0.8
 
 
 # Prompting SAM with detected boxes
-def segment(
-    sam_predictor: SamPredictor, image: np.ndarray, xyxy: np.ndarray
-) -> np.ndarray:
+def segment(sam_predictor: SamPredictor, image: np.ndarray,
+            xyxy: np.ndarray) -> np.ndarray:
     sam_predictor.set_image(image)
     result_masks = []
     for box in xyxy:
-        masks, scores, _ = sam_predictor.predict(box=box, multimask_output=True)
+        masks, scores, _ = sam_predictor.predict(box=box,
+                                                 multimask_output=True)
         index = np.argmax(scores)
         result_masks.append(masks[index])
     return np.array(result_masks)
@@ -105,50 +110,69 @@ class GroundedSAMNode(Node):
     def __init__(self, annotate: bool = False):
         super().__init__("grounded_sam_node")
 
-        self._logger = self.get_logger()
+        self.logger = self.get_logger()
 
         self.grounding_dino_model = Model(
             model_config_path=GROUNDING_DINO_CONFIG_PATH,
             model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH,
         )
         self.sam = sam_model_registry[SAM_ENCODER_VERSION](
-            checkpoint=SAM_CHECKPOINT_PATH
-        )
+            checkpoint=SAM_CHECKPOINT_PATH)
         self.sam.to(device=DEVICE)
         self.sam_predictor = SamPredictor(self.sam)
         self.openai = openai.OpenAI()
         self.assistant: Assistant = get_or_create_assistant(self.openai)
+
         self.cv_bridge = CvBridge()
+        self.moveit = MoveItPy(node_name="moveit_py")
+        self.arm = self.moveit.get_planning_component("ar_manipulator")
+        self.gripper_joint_name = "gripper_joint"
+        self.gripper_interface = GripperInterface(
+            node=self,
+            gripper_joint_names=["gripper_jaw1_joint"],
+            open_gripper_joint_positions=[-0.012],
+            closed_gripper_joint_positions=[0.0],
+            gripper_group_name="ar_gripper",
+            callback_group=ReentrantCallbackGroup(),
+            gripper_command_action_name="/gripper_controller/gripper_cmd",
+        )
 
         self.annotate = annotate
         self.n_frames_processed = 0
         self._last_depth_msg = None
         self._last_rgb_msg = None
 
-        self.image_sub = self.create_subscription(
-            Image, "/camera/color/image_raw", self.image_callback, 10
-        )
+        self.image_sub = self.create_subscription(Image,
+                                                  "/camera/color/image_raw",
+                                                  self.image_callback, 10)
         self.depth_sub = self.create_subscription(
-            Image, "/camera/aligned_depth_to_color/image_raw", self.depth_callback, 10
-        )
+            Image, "/camera/aligned_depth_to_color/image_raw",
+            self.depth_callback, 10)
 
         self.point_cloud_pub = self.create_publisher(
-            PointCloud2, "/grounded_sam/point_cloud", 2
-        )
-        self.prompt_sub = self.create_subscription(String, "/prompt", self.start, 10)
+            PointCloud2, "/grounded_sam/point_cloud", 2)
+        self.prompt_sub = self.create_subscription(String, "/prompt",
+                                                   self.start, 10)
+        self.save_images_sub = self.create_subscription(
+            String, "/save_images", self.save_images, 10)
+        self.release_at_sub = self.create_subscription(Pose, "/release_at",
+                                                       self.release_at, 10)
 
     def start(self, msg: String):
         if not self._last_rgb_msg or not self._last_depth_msg:
             return
 
-        self._logger.info(f"Processing: {msg.data}")
+        rgb_image = self.cv_bridge.imgmsg_to_cv2(self._last_rgb_msg)
+        depth_image = self.cv_bridge.imgmsg_to_cv2(self._last_depth_msg)
+
+        self.logger.info(f"Processing: {msg.data}")
         thread = self.openai.beta.threads.create()
         message = self.openai.beta.threads.messages.create(
             thread_id=thread.id,
             role="user",
             content=msg.data,
         )
-        self._logger.info(message)
+        self.logger.info(message)
 
         run = self.openai.beta.threads.runs.create_and_poll(
             thread_id=thread.id,
@@ -158,57 +182,70 @@ class GroundedSAMNode(Node):
         done = False
         while not done:
             if run.status == "completed":
-                messages = self.openai.beta.threads.messages.list(thread_id=thread.id)
-                self._logger.info(messages)
+                messages = self.openai.beta.threads.messages.list(
+                    thread_id=thread.id)
+                self.logger.info(messages)
                 done = True
                 break
             else:
-                self._logger.info(run.status)
+                self.logger.info(run.status)
 
+            detections = None
             tool_outputs = []
             for tool_call in run.required_action.submit_tool_outputs.tool_calls:
-                self._logger.info(f"tool_call: {tool_call}")
+                self.logger.info(f"tool_call: {tool_call}")
                 if tool_call.type == "function":
                     if tool_call.function.name == "get_objects":
                         args = json.loads(tool_call.function.arguments)
-                        detections = self.detect_objects(args["object_classes"])
+                        detections = self.detect_objects(
+                            rgb_image, args["object_classes"])
                         detected_classes = [
-                            CLASSES[class_id] for class_id in detections.class_id
+                            CLASSES[class_id]
+                            for class_id in detections.class_id
                         ]
-                        self._logger.info(f"Detected {detected_classes}.")
-                        tool_outputs.append(
-                            {
-                                "type": "object_detection",
-                                "object_classes": detected_classes,
-                                "confidence": detections.confidence.tolist(),
-                                "xyxy": detections.xyxy.tolist(),
-                            }
-                        )
+                        self.logger.info(f"Detected {detected_classes}.")
+                        tool_outputs.append({
+                            "type":
+                            "object_detection_result",
+                            "object_classes":
+                            detected_classes,
+                            "confidence":
+                            detections.confidence.tolist(),
+                            "xyxy":
+                            detections.xyxy.tolist(),
+                        })
                     elif tool_call.function.name == "execute_action":
                         args = json.loads(tool_call.function.arguments)
-                        self._logger.info(f"Executing action: {args['action']}")
-                        tool_outputs.append(
-                            {
-                                "type": "action",
-                                "success": True,
-                            }
+                        if detections is None:
+                            self.logger.error(
+                                "No detections available to execute action.")
+                            continue
+
+                        result = self.execute_action(
+                            args["action"],
+                            args["object_index"],
+                            detections,
+                            depth_image,
                         )
-            self._logger.info(f"tool_outputs: {tool_outputs}")
+                        tool_outputs.append({
+                            "type": "action_result",
+                            "success": result,
+                        })
+            self.logger.info(f"tool_outputs: {tool_outputs}")
 
             if tool_outputs:
                 try:
                     run = self.openai.beta.threads.runs.submit_tool_outputs_and_poll(
-                        thread_id=thread.id, run_id=run.id, tool_outputs=tool_outputs
-                    )
-                    self._logger.info("Tool outputs submitted successfully.")
+                        thread_id=thread.id,
+                        run_id=run.id,
+                        tool_outputs=tool_outputs)
+                    self.logger.info("Tool outputs submitted successfully.")
                 except Exception as e:  # pylint: disable=broad-except
-                    self._logger.error("Failed to submit tool outputs:", e)
+                    self.logger.error("Failed to submit tool outputs:", e)
             else:
-                self._logger.info("No tool outputs to submit.")
+                self.logger.info("No tool outputs to submit.")
 
-    def detect_objects(self, object_classes: str):
-        image = self.cv_bridge.imgmsg_to_cv2(self._last_rgb_msg)
-
+    def detect_objects(self, image: np.ndarray, object_classes: str):
         detections: sv.Detections = self.grounding_dino_model.predict_with_classes(
             image=image,
             classes=[object_classes],
@@ -217,15 +254,11 @@ class GroundedSAMNode(Node):
         )
 
         # NMS post process
-        nms_idx = (
-            torchvision.ops.nms(
-                torch.from_numpy(detections.xyxy),
-                torch.from_numpy(detections.confidence),
-                NMS_THRESHOLD,
-            )
-            .numpy()
-            .tolist()
-        )
+        nms_idx = (torchvision.ops.nms(
+            torch.from_numpy(detections.xyxy),
+            torch.from_numpy(detections.confidence),
+            NMS_THRESHOLD,
+        ).numpy().tolist())
 
         detections.xyxy = detections.xyxy[nms_idx]
         detections.confidence = detections.confidence[nms_idx]
@@ -244,16 +277,14 @@ class GroundedSAMNode(Node):
                 f"{CLASSES[class_id]} {confidence:0.2f}"
                 for _, _, confidence, class_id, _, _ in detections
             ]
-            annotated_frame = box_annotator.annotate(
-                scene=image.copy(), detections=detections, labels=labels
-            )
+            annotated_frame = box_annotator.annotate(scene=image.copy(),
+                                                     detections=detections,
+                                                     labels=labels)
 
-            annotated_frame = mask_annotator.annotate(
-                scene=image.copy(), detections=detections
-            )
-            cv2.imwrite(
-                f"annotated_image_{self.n_frames_processed}.jpg", annotated_frame
-            )
+            annotated_frame = mask_annotator.annotate(scene=image.copy(),
+                                                      detections=detections)
+            cv2.imwrite(f"annotated_image_{self.n_frames_processed}.jpg",
+                        annotated_frame)
 
         depth_image = self.cv_bridge.imgmsg_to_cv2(self._last_depth_msg)
         # mask out the depth image except for the detected objects
@@ -266,8 +297,7 @@ class GroundedSAMNode(Node):
         pcd = o3d.geometry.PointCloud.create_from_depth_image(
             o3d.geometry.Image(masked_depth_image),
             o3d.camera.PinholeCameraIntrinsic(
-                o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault
-            ),
+                o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault),
         )
 
         # convert it to a ROS PointCloud2 message
@@ -278,18 +308,109 @@ class GroundedSAMNode(Node):
         self.n_frames_processed += 1
         return detections
 
+    def execute_action(
+        self,
+        action: str,
+        object_index: int,
+        detections: sv.Detections,
+        depth_image: np.ndarray,
+    ) -> bool:
+        if action == "pick_object":
+            # pick the object top down
+            # mask out the depth image except for the detected objects
+            masked_depth_image = np.zeros_like(depth_image, dtype=np.float32)
+            mask = detections.mask[object_index]
+            masked_depth_image[mask] = depth_image[mask]
+            masked_depth_image /= 1000.0
+
+            # convert the masked depth image to a point cloud
+            pcd = o3d.geometry.PointCloud.create_from_depth_image(
+                o3d.geometry.Image(masked_depth_image),
+                o3d.camera.PinholeCameraIntrinsic(
+                    o3d.camera.PinholeCameraIntrinsicParameters.
+                    PrimeSenseDefault),
+            )
+            # TODO(Yifei): transform point cloud to robot base frame
+            points = np.asarray(pcd.points)
+
+            min_z = points[:, 2].min()
+            grasp_z = min_z + (points[:, 2].max() - min_z) / 2
+            # get minAreaRect of the object in top-down view
+            center, dimensions, theta = cv2.minAreaRect(points[:, :2])
+            print(center)
+            print(dimensions)
+            print(theta)
+
+        elif action == "move_above_object_and_release":
+            # move the robot arm above the object and release the object
+            pass
+        return True
+
+    def release_at(self, msg: Pose):
+        # NOTE: straight down is wxyz 0, 0, 1, 0
+        # good pose is 0, -0.3, 0.35
+        pose_goal = PoseStamped()
+        pose_goal.header.frame_id = "base_link"
+        pose_goal.pose = msg
+
+        # set plan start state to current state
+        self.arm.set_start_state_to_current_state()
+        # set pose goal with PoseStamped message
+        self.arm.set_goal_state(pose_stamped_msg=pose_goal, pose_link="link_6")
+        self._plan_and_execute()
+
+        self.gripper_interface.open()
+        self.gripper_interface.wait_until_executed()
+
+        self.gripper_interface.close()
+        self.gripper_interface.wait_until_executed()
+
     def depth_callback(self, msg):
         self._last_depth_msg = msg
 
     def image_callback(self, msg):
         self._last_rgb_msg = msg
 
+    def save_images(self, msg):
+        if not self._last_rgb_msg or not self._last_depth_msg:
+            return
+
+        rgb_image = self.cv_bridge.imgmsg_to_cv2(self._last_rgb_msg)
+        depth_image = self.cv_bridge.imgmsg_to_cv2(self._last_depth_msg)
+        print(f"depth image dtype: {depth_image.dtype}")
+        print(f"depth image max min: {depth_image.max()}, {depth_image.min()}")
+
+        save_dir = msg.data
+        cv2.imwrite(
+            os.path.join(save_dir, f"rgb_image_{self.n_frames_processed}.png"),
+            rgb_image)
+        np.save(
+            os.path.join(save_dir,
+                         f"depth_image_{self.n_frames_processed}.npz"),
+            depth_image)
+
+    def _plan_and_execute(self):
+        """Helper function to plan and execute a motion."""
+        # plan to goal
+        self.logger.info("Planning trajectory")
+        plan_result = self.arm.plan()
+
+        # execute the plan
+        if plan_result:
+            self.logger.info("Executing plan")
+            robot_trajectory = plan_result.trajectory
+            self.moveit.execute(robot_trajectory, controllers=[])
+        else:
+            self.logger.error("Planning failed")
+
 
 def main():
     rclpy.init()
     node = GroundedSAMNode()
+    executor = MultiThreadedExecutor(4)
+    executor.add_node(node)
     try:
-        rclpy.spin(node)
+        executor.spin()
     except KeyboardInterrupt:
         pass
 
--- grounded_sam_ros/grounded_sam_ros/openai_assistant.py
+++ grounded_sam_ros/grounded_sam_ros/openai_assistant.py
@@ -1,8 +1,9 @@
-from openai import OpenAI # pylint: disable=import-self
+from openai import OpenAI  # pylint: disable=import-self
 from openai.types.beta import Assistant
 
 
 ASSISTANT_ID = "asst_WLqCkCKIerFYoaSCEvCmIJdR"
+
 
 def get_or_create_assistant(client: OpenAI) -> Assistant:
     if ASSISTANT_ID:
@@ -10,8 +11,10 @@ def get_or_create_assistant(client: OpenAI) -> Assistant:
 
     return client.beta.assistants.create(
         name="Tabletop Assistant",
-        instructions=("You are a robot arm mounted on a table. Write and run code to "
-                    "do tasks on the table. You can only pick up one object at a time."),
+        instructions=(
+            "You are a robot arm mounted on a table. Write and run code to "
+            "do tasks on the table. You can only pick up one object at a time."
+        ),
         tools=[
             {
                 "type": "function",
@@ -23,16 +26,16 @@ def get_or_create_assistant(client: OpenAI) -> Assistant:
                         "properties": {
                             "object_classes": {
                                 "type": "string",
-                                "description": ("Object classes to detect, space ",
-                                                "and dot separated. For example: ",
-                                                "horses . rivers . plain")
+                                "description": (
+                                    "Object classes to detect, space ",
+                                    "and dot separated. For example: ",
+                                    "horses . rivers . plain",
+                                ),
                             }
                         },
-                        "required": [
-                            "object_classes"
-                        ]
-                    }
-                }
+                        "required": ["object_classes"],
+                    },
+                },
             },
             {
                 "type": "function",
@@ -46,21 +49,18 @@ def get_or_create_assistant(client: OpenAI) -> Assistant:
                                 "type": "string",
                                 "enum": [
                                     "pick_object",
-                                    "move_above_object_and_release"
-                                ]
+                                    "move_above_object_and_release",
+                                ],
                             },
-                            "object": {
-                                "type": "string",
-                                "description": "target object to execute the action for."
-                            }
+                            "object_index": {
+                                "type": "int",
+                                "description": "index of the object in the list of detected objects to execute the action for.",
+                            },
                         },
-                        "required": [
-                            "action",
-                            "object"
-                        ]
-                    }
-                }
-            }
+                        "required": ["action", "object"],
+                    },
+                },
+            },
         ],
         model="gpt-3.5-turbo",
     )
